e70bc84fb3e2e17bdece00b49d8fedbbb327b834
Lucas <developer.lucas@pm.me>
2025-05-11 13:39:55 -0400

    redo navbar, add navigation links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,29 @@
+import Link from "next/link";
+
 export default function NavBar({ className }: { className?: string }) {
   return (
     <div
-      className={`shadow-xl w-full grid grid-cols-2 md:grid-cols-3 p-2 bg-black/100 backdrop-blur-xl items-center ${className}`}
+      className={`shadow-xl w-full bg-black/100 backdrop-blur-xl items-center ${className}`}
     >
-      <div className="grid grid-flow-col gap-2 md:col-start-2 md:justify-center justify-start">
-        <button className="p-1">Projects</button>
-        <button className="p-1">Resume</button>
-        <button className="p-1">Contact</button>
+      <div className="max-w-4xl mx-auto flex justify-between items-center py-3 px-6 md:px-0">
+        <Link href="/" className="text-2xl font-bold hover:text-blue-500">
+          Lucas Anderson
+        </Link>
+        <div className="space-x-6 text-lg">
+          <Link href="#about" className="hover:text-blue-500">
+            About
+          </Link>
+          <Link href="#projects" className="hover:text-blue-500">
+            Projects
+          </Link>
+          <Link
+            href="/resume.pdf"
+            target="_blank"
+            className="hover:text-blue-500"
+          >
+            Résumé
+          </Link>
+        </div>
       </div>
     </div>
   );
